3e29f88cf5d3f5e40c1e84f99e58e501748f9aaa
Adrienne <iliu.adrienne@gmail.com>
2019-06-24 20:57:51 -0700

    display bamazon database in node terminal

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
+//npm packages
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+//connect to mysql workbench
 var connection = mysql.createConnection({
     host: "localhost", 
     port: 3306,
@@ -9,3 +11,31 @@ var connection = mysql.createConnection({
     password: "password",
     database: "bamazon"
 });
+
+connection.connect(function(err) {
+    if (err) throw err; 
+    //console.log("connected as id " + connection.threadId + "\n");
+    initial(); 
+    connection.end();
+});
+
+//what the user sees after starting 
+function initial() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err; 
+
+        for (var i = 0; i<res.length; i++) {
+            console.log(res[i].item_id + "| " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
+        }
+
+        inquirer.prompt([ 
+            {
+                type: "input",
+                message: "What is the ID of the item you'd like to order?",
+                name: "userPick"
+            }
+        ]).then(function(result) {
+            console.log(result.userPick);
+        }) 
+    })
+}
--- bamazonDB.sql
+++ bamazonDB.sql
@@ -6,11 +6,17 @@ CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100),
     department_name VARCHAR(100),
-    price INTEGER (10,2),
+    price DECIMAL (10,2),
     stock_quantity INTEGER(100),
-    PRIMARY KEY(id)
+    PRIMARY KEY(item_id)
 );
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("FrixOn", "Stationary", 3, 10), 
-    ("Pentel", "Stationary", 1, 10);
+VALUES ("Spaghetti", "Pasta", 3, 10), 
+    ("Lasagna", "Pasta", 1, 10),
+    ("Tonkatsu Ramen", "Noodle", 5, 10),
+    ("Sashimi", "Fish", 8, 10),
+    ("Kit-Kat", "Sweets", 2, 10), 
+    ("Hot Dog", "Meat", 3, 10), 
+    ("Steal", "Meat", 7, 10)
+    ;
