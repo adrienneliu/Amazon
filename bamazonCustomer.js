ef04b84342850136da49c6ca356f372d55338d52
Adrienne <iliu.adrienne@gmail.com>
2019-06-26 14:57:08 -0700

    get rid of duplicate table with updated stock quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,7 +7,7 @@ var Table = require('cli-table2');
 //table 
 var table = new Table({
     head: ['ID', 'Item', 'Cuisine', 'Price', 'Quantity']
-  , colWidths: [15, 25, 25, 15, 15]
+    , colWidths: [15, 25, 25, 15, 15]
 });
 
 
@@ -25,19 +25,23 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
-    initial();
+    initialDisplay();
 
 });
 
+//input we need to grab
+var quantityFinal;
+var productID;
+
 //what the user sees after starting 
-function initial() {
+function initialDisplay() {
 
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
 
 
         //insert story line here ... REPLACE WORDS
-        console.log("\n WHAT DO YOU WANNA EAT BISH \n");
+        console.log("\n WHAT DO YOU WANNA EAT \n");
 
         for (var i = 0; i < res.length; i++) {
             // console.log(res[i].item_id + "| " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
@@ -47,37 +51,102 @@ function initial() {
                 [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
             );
 
-            
-        }console.log(table.toString());
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "What is the ID of the item you'd like to order?",
-                name: "userPick"
-            },
-            {
-                type: "input",
-                message: "How many do you want?",
-                name: "amount"
-            }
-        ]).then(function (result) {
-            //console.log(result.userPick);
-            //console.log(result.amount);
 
-            var query = "SELECT * FROM products WHERE item_id = ?";
-            connection.query(query, [result.userPick], function (err, res) {
-                if (err) throw err;
+        } 
+        console.log(table.toString());
+        prompt();
+       
+    });
+}
 
-                var quantityFinal = res[0].stock_quantity - result.amount;
+function prompt() {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the ID of the item you'd like to order?",
+            name: "userPick"
+        },
+        {
+            type: "input",
+            message: "How many do you want?",
+            name: "amount"
+        }
+    ]).then(function (result) {
+        //console.log(result.userPick);
+        //console.log(result.amount);
+        productID = result.userPick;
 
-                console.log("You ordered " + result.amount + " " + res[0].product_name + "\n");
-                console.log(res[0].product_name + " | " + res[0].department_name + " | " + res[0].price + " | " + quantityFinal);
-            })
+        var query = "SELECT * FROM products WHERE item_id = ?";
+        connection.query(query, [productID], function (err, res) {
+            if (err) throw err;
+
+            quantityFinal = res[0].stock_quantity - result.amount;
+
+            console.log(productID)
+            console.log(quantityFinal)
+
+            // console.log("You ordered " + result.amount + " " + res[0].product_name + "\n");
+            // console.log(res[0].product_name + " | " + res[0].department_name + " | " + res[0].price + " | " + quantityFinal);
+            updateProduct(quantityFinal, productID);
         })
+        // })
     })
 }
-//list items
-//prompt which id and how many they pick
-//take the id and quantity they input
-//update object with unique id by price/quantity
-//display results
+function updateProduct(quantityFinal, productID) {
+    console.log(productID);
+    console.log(quantityFinal);
+
+    var query = connection.query("UPDATE products SET ? WHERE ?",
+
+        [
+            {
+                stock_quantity: quantityFinal
+            },
+            {
+                item_id: productID
+            }
+        ], function (err, res) {
+            if (err) throw err;
+
+            console.log(res.affectedRows + " products updated");
+
+            if (productID===10){
+                console.log("Pick another number")
+                connection.end();
+            } else{
+
+            readDisplay();
+        }
+            // for (var i = 0; i<res.length; i++) {
+            //     table.push(
+            //         [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            //     );
+            // }console.log(table.toString());
+        } 
+    );
+}
+
+function readDisplay() {
+    console.log("Show");
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        
+        table = new Table({
+            head: ['ID', 'Item', 'Cuisine', 'Price', 'Quantity']
+            , colWidths: [15, 25, 25, 15, 15]
+        });
+       
+        for (var i = 0; i < res.length; i++) {
+
+            // table is an Array, so you can `push`, `unshift`, `splice` and friends
+            table.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+
+
+        } 
+        console.log(table.toString());
+
+        connection.end();
+    })
+}
--- bamazonDB.sql
+++ bamazonDB.sql
@@ -6,17 +6,22 @@ CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100),
     department_name VARCHAR(100),
-    price DECIMAL (10,2),
+    price DECIMAL (10,20),
     stock_quantity INTEGER(100),
     PRIMARY KEY(item_id)
 );
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
-VALUES ("Spaghetti", "Pasta", 3, 10), 
-    ("Lasagna", "Pasta", 1, 10),
-    ("Tonkatsu Ramen", "Noodle", 5, 10),
-    ("Sashimi", "Fish", 8, 10),
-    ("Kit-Kat", "Sweets", 2, 10), 
-    ("Hot Dog", "Meat", 3, 10), 
-    ("Steak", "Meat", 7, 10)
+VALUES ("Spaghetti", "Italian", 7.50, 50), 
+    ("Lasagna", "Italian", 7.50, 50),
+    ("Tonkatsu Ramen", "Japanese", 6.99, 35),
+    ("Sashimi", "Japanese", 17.99, 15),
+    ("Kit-Kat", "Sweets", 0.75, 100), 
+    ("Hot Dog", "American", 1.50, 100), 
+    ("Steak", "American", 27.99, 55),
+    ("Katsu", "Japanese", 13.50, 65),
+    ("Pancake", "American", 3.50, 50), 
+    ("Blueberry Pancake", "American", 5.50, 50), 
+    ("Joey Tacos", "Mexican", 1.50, 8)
+
     ;
