681faec68cf960b72259e71e27c345530c685344
Adrienne <iliu.adrienne@gmail.com>
2019-06-26 21:35:50 -0700

    add message if user orders more than stock quantity

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,6 +2,10 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var Table = require('cli-table2');
+var chalk = require("chalk");
+
+const chalkAnimation = require('chalk-animation');
+
 
 
 //table 
@@ -9,8 +13,6 @@ var table = new Table({
     head: ['ID', 'Item', 'Cuisine', 'Price', 'Quantity']
     , colWidths: [15, 25, 25, 15, 15]
 });
-
-
 
 //connect to mysql workbench
 var connection = mysql.createConnection({
@@ -39,7 +41,6 @@ function initialDisplay() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
 
-
         //insert story line here ... REPLACE WORDS
         console.log("\n WHAT DO YOU WANNA EAT \n");
 
@@ -50,12 +51,9 @@ function initialDisplay() {
             table.push(
                 [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
             );
-
-
-        } 
+        }
         console.log(table.toString());
         prompt();
-       
     });
 }
 
@@ -64,12 +62,14 @@ function prompt() {
         {
             type: "input",
             message: "What is the ID of the item you'd like to order?",
-            name: "userPick"
+            name: "userPick",
+            filter: Number
         },
         {
             type: "input",
             message: "How many do you want?",
-            name: "amount"
+            name: "amount",
+            filter: Number
         }
     ]).then(function (result) {
         //console.log(result.userPick);
@@ -82,22 +82,25 @@ function prompt() {
 
             quantityFinal = res[0].stock_quantity - result.amount;
 
-            console.log(productID)
-            console.log(quantityFinal)
-
-            // console.log("You ordered " + result.amount + " " + res[0].product_name + "\n");
-            // console.log(res[0].product_name + " | " + res[0].department_name + " | " + res[0].price + " | " + quantityFinal);
+            if (quantityFinal  < 0) {
+                console.log("Sorry, we ran out!");
+                prompt();
+            } else{
+            console.log("You ordered " + result.amount + " " + (chalk.blue(res[0].product_name)) + ".\n Thank you, that wil be $" + (chalk.green(result.amount * res[0].price)) + "."
+            );
             updateProduct(quantityFinal, productID);
+            }
+            
         })
         // })
     })
 }
+
 function updateProduct(quantityFinal, productID) {
-    console.log(productID);
-    console.log(quantityFinal);
+    // console.log(productID);
+    // console.log(quantityFinal);
 
     var query = connection.query("UPDATE products SET ? WHERE ?",
-
         [
             {
                 stock_quantity: quantityFinal
@@ -107,35 +110,21 @@ function updateProduct(quantityFinal, productID) {
             }
         ], function (err, res) {
             if (err) throw err;
-
-            console.log(res.affectedRows + " products updated");
-
-            if (productID===10){
-                console.log("Pick another number")
-                connection.end();
-            } else{
-
             readDisplay();
         }
-            // for (var i = 0; i<res.length; i++) {
-            //     table.push(
-            //         [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
-            //     );
-            // }console.log(table.toString());
-        } 
     );
 }
 
 function readDisplay() {
-    console.log("Show");
-    connection.query("SELECT * FROM products", function(err, res) {
+    //console.log("Show");
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        
+
         table = new Table({
             head: ['ID', 'Item', 'Cuisine', 'Price', 'Quantity']
             , colWidths: [15, 25, 25, 15, 15]
         });
-       
+
         for (var i = 0; i < res.length; i++) {
 
             // table is an Array, so you can `push`, `unshift`, `splice` and friends
@@ -143,10 +132,10 @@ function readDisplay() {
                 [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
             );
 
-
-        } 
+        }
         console.log(table.toString());
-
         connection.end();
+chalkAnimation.glitch("Thanks for your purchase!");
+       
     })
 }
--- bamazonDB.sql
+++ bamazonDB.sql
@@ -6,22 +6,40 @@ CREATE TABLE products (
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100),
     department_name VARCHAR(100),
-    price DECIMAL (10,20),
+    price DECIMAL (10,2),
     stock_quantity INTEGER(100),
     PRIMARY KEY(item_id)
 );
 
 INSERT INTO products(product_name, department_name, price, stock_quantity)
 VALUES ("Spaghetti", "Italian", 7.50, 50), 
-    ("Lasagna", "Italian", 7.50, 50),
-    ("Tonkatsu Ramen", "Japanese", 6.99, 35),
+
     ("Sashimi", "Japanese", 17.99, 15),
     ("Kit-Kat", "Sweets", 0.75, 100), 
     ("Hot Dog", "American", 1.50, 100), 
     ("Steak", "American", 27.99, 55),
-    ("Katsu", "Japanese", 13.50, 65),
+    ("Curry Katsu", "Japanese", 13.50, 65),
     ("Pancake", "American", 3.50, 50), 
     ("Blueberry Pancake", "American", 5.50, 50), 
-    ("Joey Tacos", "Mexican", 1.50, 8)
+    ("Joey Tacos", "Mexican", 1.50, 8),
+    ("Chloe Hot Pot", "Chinese", 18.99, 20),
+    ("Shabu Shabu", "Japanese", 23, 20), 
+    ("Reg Biryani", "Indian", 10.00, 35),
+    ("Esar's Mom's Biryani", "Indian", 999, 1),
+    ("Mac and Cheese", "American", 6.00, 100),
+    ("XLB", "Chinese", 6.00, 100),
+    ("Green Curry", "Thai", 7.00, 30)
 
     ;
+
+    --     ("Musubi", "Hawaiian", 1.00, 100),
+    -- ("Pho", "Vietnamese", 7.50, 50),
+    -- ("Sui Mai 3pc", "Chinese", 0.75, 100), 
+    -- ("Ice Cream", "Sweets", 2.99, 100), 
+    -- ("Chocolate Cake 1pc", "Sweets", 1.50, 50),
+    -- ("Banh Mi", "Vietnamese", 4.00, 75), 
+    -- ("Loco Moco", "Hawaiian", 8.00, 100),
+    --     ("Lasagna", "Italian", 7.50, 50),
+    -- ("Tonkatsu Ramen", "Japanese", 6.99, 35),
+    --     ("Robot Bibimbap", "Korean", 12.50, 70), 
+    -- ("Kimbap", "Korean", 1.50, 100),
